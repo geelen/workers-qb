09e7a11c8165dee2bf437a7d487298d21893aa4f
Glen Maddern <glen@cloudflare.com>
2022-10-24 14:45:20 +1100

    added batching

--- src/Databases.ts
+++ src/Databases.ts
@@ -1,36 +1,63 @@
-import {QueryBuilder} from "./Builder";
-import {FetchTypes} from "./enums";
+import { QueryBuilder } from './Builder'
+import { FetchTypes } from './enums'
+import { Params } from './interfaces'
 
-type Params = { query: String, arguments?: (string | number | boolean | null)[], fetchType?: FetchTypes };
-const now = typeof performance === 'undefined' ? () => Date.now() : () => performance.now()
+// @ts-ignore
+const now = typeof globalThis.performance === 'undefined' ? () => Date.now() : () => globalThis.performance.now()
 
 export class D1QB extends QueryBuilder {
-  private db: any;
-  constructor(db: any) {
-    super();
-    this.db = db;
+  private db: D1Database
+  constructor(db: D1Database) {
+    super()
+    this.db = db
+  }
+
+  async batch(cb: (batchedDB: QueryBuilder) => Promise<any>[]): Promise<{ results: any[]; meta: any }> {
+    const batchedDB = new BatchedQB()
+    cb(batchedDB)
+    console.log(batchedDB.queries)
+
+    const prev = now()
+    const results = await this.db.batch(
+      batchedDB.queries.map((queued) => {
+        return this._prepare(queued.params)
+      })
+    )
+    const end = {
+      query: 'BATCH',
+      worker_duration: now() - prev,
+    }
+    const meta: any[] = []
+
+    results.forEach((r, i) => {
+      batchedDB.queries[i].resolve(r)
+      meta.push({
+        ...batchedDB.queries[i].params,
+        db_duration: r.duration,
+        served_by: r.served_by,
+      })
+    })
+    meta.push(end)
+    return { results, meta }
   }
 
   async execute(params: Params): Promise<any> {
     const prev = now()
-    const result = await this._execute(params);
+    const result = await this._execute(params)
+    const meta = {
+      ...params,
+      db_duration: result.duration,
+      served_by: result.served_by,
+      worker_duration: now() - prev,
+    }
     return {
       ...result,
-      meta: {
-        ...params,
-        db_duration: result.duration,
-        served_by: result.served_by,
-        worker_duration: now() - prev
-      }
+      meta,
     }
   }
 
   async _execute(params: Params) {
-    let stmt = this.db.prepare(params.query)
-
-    if (params.arguments) {
-      stmt = stmt.bind(...params.arguments)
-    }
+    const stmt = this._prepare(params)
 
     if (params.fetchType === FetchTypes.ONE) {
       return stmt.first()
@@ -39,5 +66,34 @@ export class D1QB extends QueryBuilder {
     }
 
     return stmt.run()
+  }
+
+  private _prepare(params: Params) {
+    let stmt = this.db.prepare(params.query)
+
+    if (params.arguments) {
+      stmt = stmt.bind(...params.arguments)
+    }
+    return stmt
+  }
+}
+
+type QueuedQuery = {
+  params: Params
+  resolve: (value: unknown) => void
+  reject: (value: unknown) => void
+}
+
+class BatchedQB extends QueryBuilder {
+  queries: QueuedQuery[] = []
+
+  async execute(params: Params): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.queries.push({
+        params,
+        resolve,
+        reject,
+      })
+    })
   }
 }
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,9 +1,11 @@
-import {OrderTypes} from "./enums";
+import { FetchTypes, OrderTypes } from "./enums";
+
+type Value = string | boolean | number | null
 
 export interface Where {
   conditions: string | Array<string>
-  // TODO: enable named parameters Record<string, string | boolean | number | null>
-  params?: (string | boolean | number | null)[]
+  // TODO: enable named parameters Record<string, Value>
+  params?: (Value)[]
 }
 
 export interface SelectOne {
@@ -20,15 +22,23 @@ export interface SelectAll extends SelectOne{
   limit?: number
 }
 
-export interface Insert{
+export type SingleRow = {
+  data: Record<string, Value>
+};
+export type MultipleRows = {
+  columns: string[],
+  rows: Value[][]
+};
+export type SingleOrMultiple = SingleRow | MultipleRows
+
+export type Insert = SingleOrMultiple & {
     tableName: string
-    data: Record<string, string | boolean | number | null>
     returning?: string | Array<string>
 }
 
 export interface Update{
     tableName: string
-    data: Record<string, string | boolean | number | null>
+    data: Record<string, Value>
     where: Where
     returning?: string | Array<string>
 }
@@ -39,20 +49,34 @@ export interface Delete{
     returning?: string | Array<string>
 }
 
+export interface Params{
+    query: string,
+    arguments?: (string | number | boolean | null)[],
+    fetchType?: FetchTypes
+};
+
+export interface Meta extends Params{
+    db_duration: number,
+    worker_duration: number,
+    served_by: string
+}
+
 export interface Result{
     changes?: number
     duration: number
     lastRowId?: number
-    results?: Array<Record<string, string | boolean | number | null>>
+    results?: Array<Record<string, Value>>
     served_by: string
     success: boolean
+    meta: Meta
 }
 
 export interface ResultOne{
     changes?: number
     duration: number
     lastRowId?: number
-    results?: Record<string, string | boolean | number | null>
+    results?: Record<string, Value>
     served_by: string
     success: boolean
+    meta: Meta
 }
