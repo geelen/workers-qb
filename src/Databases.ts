8ed81bc17ea78f38bbaccc9733d97a6c13fe9dd7
Glen Maddern <glen@cloudflare.com>
2022-10-24 11:04:04 +1100

    capturing metadata for perf testing

--- src/Databases.ts
+++ src/Databases.ts
@@ -1,6 +1,9 @@
 import {QueryBuilder} from "./Builder";
 import {FetchTypes} from "./enums";
 
+type Params = { query: String, arguments?: (string | number | boolean | null)[], fetchType?: FetchTypes };
+const now = typeof performance === 'undefined' ? () => Date.now() : () => performance.now()
+
 export class D1QB extends QueryBuilder {
   private db: any;
   constructor(db: any) {
@@ -8,7 +11,21 @@ export class D1QB extends QueryBuilder {
     this.db = db;
   }
 
-  async execute(params: {query: String, arguments?: (string | number | boolean | null)[], fetchType?: FetchTypes}): Promise<any> {
+  async execute(params: Params): Promise<any> {
+    const prev = now()
+    const result = await this._execute(params);
+    return {
+      ...result,
+      meta: {
+        ...params,
+        db_duration: result.duration,
+        served_by: result.served_by,
+        worker_duration: now() - prev
+      }
+    }
+  }
+
+  async _execute(params: Params) {
     let stmt = this.db.prepare(params.query)
 
     if (params.arguments) {
